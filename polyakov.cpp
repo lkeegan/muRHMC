8d20cc4761c9c985a14d468ce349c0baf6690ae8
lkeegan <liam@keegan.ch>
2018-03-22 11:30:55 +0100

    file missing

---
+++ polyakov.cpp
@@ -0,0 +1,43 @@
+#include "hmc.hpp"
+#include "io.hpp"
+#include "stats.hpp"
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+
+    if (argc-1 != 4) {
+        std::cout << "This program requires 4 arguments:" << std::endl;
+        std::cout << "base_name initial_config rho n_smear" << std::endl;
+        std::cout << "e.g. ./polyakov mu0.25_sus_3.1_3.3 1 0.05 10" << std::endl;
+        return 1;
+    }
+
+	std::string base_name(argv[1]);
+	int n_initial = static_cast<int>(atof(argv[2]));
+	double rho = atof(argv[3]);
+	int n_smear = static_cast<int>(atof(argv[4]));
+
+	hmc_params hmc_pars;
+	hmc_pars.seed = 123;
+
+	// make 12^4 lattice
+	lattice grid (12);
+	hmc hmc (hmc_pars);
+	field<gauge> U (grid);
+	std::cout.precision(12);
+
+	log("Polyakov loop measurements with parameters:");
+	log("L", grid.L0);
+	log("rho", rho);
+	log("n_smear", n_smear);
+
+	for(int i=n_initial; ; i+=1) {
+		read_gauge_field(U, base_name, i);
+		std::cout << i << "n_smear = " << 0 << "\t" << hmc.polyakov_loop(U) << std::endl;
+		for(int i_smear=1; i_smear<n_smear; ++i_smear) {
+			hmc.stout_smear(rho, U);
+			std::cout << i << "n_smear = " << i_smear << "\t" << hmc.polyakov_loop(U) << std::endl;
+		}
+	}
+	return(0);
+}
