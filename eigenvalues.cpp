ba9298c74e1ee921cb57fc6768ed3530b0b0414b
lkeegan <liam@keegan.ch>
2018-07-19 09:45:38 +0200

    lowest n eigenvalues

--- eigenvalues.cpp
+++ eigenvalues.cpp
@@ -9,22 +9,22 @@
 int main(int argc, char *argv[]) {
   std::cout.precision(17);
 
-  if (argc - 1 != 5) {
-    std::cout << "This program requires 5 arguments:" << std::endl;
-    std::cout << "mass mu_I base_name config_number relative_error"
-              << std::endl;
-    std::cout << "e.g. ./hmc 0.14 0.25 mu0.25_sus_3.1_3.3 1 1e-4" << std::endl;
+  constexpr int n_args = 4;
+  if (argc - 1 != n_args) {
+    std::cout << "This program requires " << n_args
+              << " arguments:" << std::endl;
+    std::cout << "mass L base_name config_number" << std::endl;
+    std::cout << "e.g. ./eigenvalues 8 0.002 conf 1" << std::endl;
     return 1;
   }
 
-  double mass = atof(argv[1]);
-  double mu_I = atof(argv[2]);
+  int L = static_cast<int>(atof(argv[1]));
+  double mass = atof(argv[2]);
   std::string base_name(argv[3]);
   int config_number = static_cast<int>(atof(argv[4]));
-  int N_eigenvalues = N_rhs;  // static_cast<int>(atof(argv[5]));
-  double eps = atof(argv[5]);
+  int N_eigenvalues = N_rhs;
 
-  lattice grid(8, 8, true);
+  lattice grid(L, L, true);
   // field<fermion>::eo_storage_options eo_storage = field<fermion>::FULL;
   field<fermion>::eo_storage_options eo_storage = field<fermion>::EVEN_ONLY;
 
@@ -32,13 +32,11 @@ int main(int argc, char *argv[]) {
   log("T", grid.L0);
   log("L", grid.L1);
   log("mass", mass);
-  log("mu_I", mu_I);
   log("N_eigenvalues", N_eigenvalues);
-  log("relative_error", eps);
 
   field<gauge> U(grid);
   read_gauge_field(U, base_name, config_number);
-  dirac_op D(grid, mass, mu_I);
+  dirac_op D(grid, mass);
   rhmc_params rhmc_pars;
   rhmc_pars.seed = 123;
   rhmc rhmc(rhmc_pars, grid);
@@ -52,7 +50,7 @@ int main(int argc, char *argv[]) {
   double lambda_max = 1;
   double lambda_max_err = 100;
   int iter = 0;
-  while ((lambda_max_err / lambda_max) > eps) {
+  while ((lambda_max_err / lambda_max) > 1e-6) {
     for (int i = 0; i < 8; ++i) {
       x /= x_norm;
       D.DDdagger(x2, x, U);
@@ -61,14 +59,66 @@ int main(int argc, char *argv[]) {
       x_norm = x.norm();
       iter += 2;
     }
-    lambda_max = x2.real_dot(x);
+    lambda_max = x2.dot(x).real();
     lambda_max_err = sqrt(x_norm * x_norm - lambda_max * lambda_max);
+    // log("lambda_max", lambda_max, lambda_max_err);
   }
   // since lambda_max is a lower bound, and lambda_max + lamda_max_err is an
   // upper bound:
-  std::cout << "iterations" << iter << std::endl;
-  std::cout << "final_lambda_max" << lambda_max + 0.5 * lambda_max_err << "\t"
-            << 0.5 * lambda_max_err << std::endl;
+  // log("iterations", iter);
+  // log("final_lambda_max", lambda_max + 0.5 * lambda_max_err,
+  //  0.5 * lambda_max_err);
 
+  // Find N lowest eigenvalues of DDdagger.
+  // Uses chebyshev acceleration as described in Appendix A of hep-lat/0512021
+  block_matrix R = block_matrix::Zero();
+  Eigen::MatrixXd Evals = Eigen::MatrixXd::Zero(N_eigenvalues, 2);
+
+  // make initial fermion vector basis of gaussian noise vectors
+  field<block_fermion> X(grid, field<block_fermion>::EVEN_ONLY);
+  rhmc.gaussian_fermion(X);
+  // orthonormalise X
+  thinQR(X, R);
+  // make X A-orthormal and get eigenvalues of matrix <X_i AX_j>
+  thinQRA_evals(X, Evals, U, D);
+
+  double delta = 1.0;  // change from last estimate of nth eigenvalue
+  // v is the upper bound on possible eigenvalues
+  // use 50% margin of safety on estimate of error to get safe upper bound
+  double v = lambda_max + 1.5 * lambda_max_err;
+  while (delta > 1.e-10) {
+    // get current estimates of min/max eigenvalues
+    double lambda_min = Evals.col(0)[0];
+    double lambda_N = Evals.col(0)[N_eigenvalues - 1];
+    // find optimal chebyshev order k
+    int k = 2 * std::ceil(0.25 * sqrt(((v - lambda_min) * 8.33633354 -
+                                       (v - lambda_N) * 3.1072776) /
+                                      (lambda_N - lambda_min)));
+    // find chebyshev lower bound
+    double u = lambda_N + (v - lambda_N) * (tanh(1.76275 / (2.0 * k))) *
+                              tanh(1.76275 / (2.0 * k));
+    log("Chebyshev order k:", k);
+    log("Chebyshev range u:", u);
+    log("Chebyshev range v:", v);
+    // apply chebyshev polynomial in DDdagger
+    D.chebyshev(k, u, v, X, U);
+    // orthonormalise X
+    thinQR(X, R);
+    // make X A-orthormal and get eigenvalues of matrix <X_i AX_j>
+    thinQRA_evals(X, Evals, U, D);
+    // note the estimated errors for the eigenvalues are only correct
+    // if the eigevalues are separated more than the errors
+    // i.e. assumes residuals matrix is diagonal
+    // if this is not the case then errors are underestimated
+    delta = fabs((lambda_N - Evals.col(0)[N_eigenvalues - 1]) / lambda_N);
+    log("relative change:", delta);
+    // std::cout << Evals << std::endl;
+  }
+
+  std::cout << 0 << "\t" << sqrt(lambda_max + 0.5 * lambda_max_err)
+            << std::endl;
+  for (int i = 0; i < N_eigenvalues; ++i) {
+    std::cout << i + 1 << "\t" << sqrt(Evals.col(0)[i]) << std::endl;
+  }
   return (0);
 }
